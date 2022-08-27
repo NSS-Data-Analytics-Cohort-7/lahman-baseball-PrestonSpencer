c773545ba32a7042e2bc36dd7e1f595f14e018f1
PrestonSpencer <prestonspencer3@gmail.com>
2022-08-27 13:56:38 -0500

    Q11,12

--- Scripts/lahman_scripts.sql
+++ Scripts/lahman_scripts.sql
@@ -166,6 +166,7 @@ JOIN team
 ON parks.team = team.teamid
 GROUP BY parks.park_name, team.franchname, attendance_per_game
 ORDER BY attendance_per_game DESC
+LIMIT 5
 
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
 -- ANSWER: 
@@ -253,11 +254,11 @@ ORDER BY salaries.yearid, total_team_salary DESC, w DESC
 -- 12. In this question, you will explore the connection between number of wins and attendance. Does there appear to be any correlation between attendance at home games and number of wins? Do teams that win the world series see a boost in attendance the following year? What about teams that made the playoffs? Making the playoffs means either being a division winner or a wild card winner.
 -- ANSWER:
 
-SELECT yearid, teamid, w, attendance
+SELECT yearid, teamid, w, attendance, wswin, divwin, wcwin
 FROM teams
-GROUP BY yearid, teamid, w, attendance
-ORDER BY yearid
-    
+GROUP BY yearid, teamid, w, attendance, wswin, divwin, wcwin
+ORDER BY yearid DESC
 
+    
 
 /* 13. It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
